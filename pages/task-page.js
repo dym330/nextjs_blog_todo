58afa68e45ae0b263af5798fbbdf304fede41e10
YukiYamada <cat.of.attic@gmail.com>
2021-06-22 18:54:42 +0900

    finish

--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,8 @@ import { getAllTasksData } from "../lib/tasks";
 import Task from "../components/Task";
 import useSWR from "swr";
 import { useEffect } from "react";
+import StateContextProvider from "../context/StateContext";
+import TaskForm from "../components/TaskForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
@@ -14,37 +16,42 @@ export default function TaskPage({ staticfilterdTasks }) {
   });
   const fillteredTasks = tasks?.sort(
     (a, b) => new Date(b.created_at) - new Date(a.created_at)
-  )
+  );
   useEffect(() => {
     mutate(); //useSWRで取得できるキャッシュを最新の状態にする
   }, []);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {fillteredTasks &&
-          fillteredTasks.map((task) => <Task key={task.id} task={task} />)}
-      </ul>
-      <Link href="/main-page">
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mr-3"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to Main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <TaskForm taskCreated={mutate}/>
+        <ul>
+          {fillteredTasks &&
+            fillteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page">
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to Main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 export async function getStaticProps() {
